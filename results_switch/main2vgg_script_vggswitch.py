a543c89d707cc7e395fa8cd39012c97f8f0f13c3
Kamil Adamczewski <kamil.m.adamczewski@gmail.com>
2020-03-02 20:17:02 +0100

    minor

--- results_switch/main2vgg_script_vggswitch.py
+++ results_switch/main2vgg_script_vggswitch.py
@@ -1,6 +1,21 @@
 import subprocess
 from main2vgg_switch_integral_work import main as main_integral
 import numpy as np
+import os
+import socket
+
+#######
+# path stuff
+cwd = os.getcwd()
+if 'g0' in socket.gethostname() or 'p0' in socket.gethostname():
+    #the cwd is where the sub file is so ranking/
+    path_switch = os.path.join(cwd, "results_switch")
+    path_main= cwd
+else:
+    #the cwd is results_compression
+    parent_path = os.path.abspath('..')
+    path_switch = cwd
+    path_main= parent_path
 
 print("v4")
 
@@ -11,16 +26,16 @@ print("v4")
 alpha=0.05; switch_init=0.05
 epochs_num=1
 dataset='cifar'
-num_samps_for_switch=4
+num_samps_for_switch=2
 
-file_path='results/switch_data_%s_9032_integral_samps_%s_epochs_%i.npy' % (dataset, str(num_samps_for_switch), epochs_num)
+file_path=path_switch+'/results/switch_data_%s_9032_integral_samps_%s_epochs_%i.npy' % (dataset, str(num_samps_for_switch), epochs_num)
 #file_path=os.path.join(path_main, 'results_switch/results/switch_data_%s_9032_integral_samps_%s_epochs_%i.npy' % (dataset, str(num_samps_for_switch), epochs_num))
 
 
 if 1:
     if 1:
         switch_data={}; switch_data['combinationss'] = []; switch_data['switches']=[]
-        for i in range(2,3):
+        for i in range(1,2):
             switch_layer='conv'+str(i)
             #subprocess.call(['/home/kadamczewski/miniconda3/envs/BayesianNeuralNetwork/bin/python', 'main2vgg_switch.py', switch_layer, str(alpha), str(switch_init)])
             #subprocess.call(['python', 'main2vgg_switch.py', switch_layer, str(alpha), str(switch_init)])
@@ -29,7 +44,7 @@ if 1:
             print(ranks, switches)
             #switch_data['combinationss'].append(ranks);
             #switch_data['switches'].append(switches)
-        np.save(file_path, switch_data)
+            np.save(file_path, switch_data)
 
 
 
